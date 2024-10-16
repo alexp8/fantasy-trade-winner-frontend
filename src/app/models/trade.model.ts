9c74019e8e6515875200efd042e0eb0743543027
Alex <amp1993@gmail.com>
2024-10-15 22:32:35 -0400

    updates

--- src/app/models/trade.model.ts
+++ src/app/models/trade.model.ts
@@ -1,55 +1,56 @@
 export interface PreviousLeague {
-    previous_league_id: string;
-    season: string;
-  }
-  
-  export interface Player {
-    player_id: string;
-    player_name: string;
-    value_when_traded: number;
-    latest_value: number;
-    value_now_as_of: string | null;
-  }
-  
-  export interface DraftPick {
-    round: number;
-    range: string;
-    season: string;
-    value_when_traded: number;
-    latest_value: number;
-    value_now_as_of: string | null;
-    description: string;
-  }
-  
-  export interface TradeRoster {
-    total_current_value: number;
-    total_value_when_traded: number;
-    user_name: string;
-    avatar_url: string;
-    user_id: string;
-    roster_id: number;
-    fab: number;
-    players: Player[];
-    draft_picks: DraftPick[];
-    won: Boolean;
-  }
-  
-  export interface Trade {
-    [rosterId: number]: TradeRoster;
-    created_at_millis: number;
-    created_at_formatted: string;
-    transaction_id: string;
-    roster_ids: number[];
-    week: number;
-  }
-  
-  export interface TradeResponse {
-    page: number;
-    total_pages: number;
-    total_trades: number;
-    has_next: boolean;
-    has_previous: boolean;
-    previous_league_ids: PreviousLeague[];
-    trades: Trade[];
-  }
-  
+  previous_league_id: string;
+  season: string;
+}
+
+export interface Player {
+  player_id: string;
+  player_name: string;
+  value_when_traded: number;
+  latest_value: number;
+  value_now_as_of: string | null;
+}
+
+export interface DraftPick {
+  round: number;
+  range: string;
+  season: string;
+  value_when_traded: number;
+  latest_value: number;
+  value_now_as_of: string | null;
+  description: string;
+}
+
+export interface TradeRoster {
+  total_current_value: number;
+  total_value_when_traded: number;
+  user_name: string;
+  avatar_url: string;
+  user_id: string;
+  roster_id: number;
+  fab: number;
+  players: Player[];
+  draft_picks: DraftPick[];
+  won: Boolean;
+}
+
+export interface Trade {
+  [rosterId: number]: TradeRoster;
+  created_at_millis: number;
+  created_at_formatted: string;
+  transaction_id: string;
+  roster_ids: number[];
+  week: number;
+}
+
+export interface TradeResponse {
+  league_name: String;
+  league_season: String;
+  page: number;
+  total_pages: number;
+  total_trades: number;
+  has_next: boolean;
+  has_previous: boolean;
+  previous_league_ids: PreviousLeague[];
+  trades: Trade[];
+}
